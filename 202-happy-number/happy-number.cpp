15b5e60bf1d85584ab95d7036e32fab98c9260e3
Charandeep Singh <110605970+CharandeepSingh01@users.noreply.github.com>
2026-07-13 00:41:03 +0530

    Time: 3 ms (15.65%) | Memory: 8.4 MB (22.72%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        int fast = nextsum(n), slow = n;
-        while(fast != 1 && fast != slow){
-            slow = nextsum(slow);
-            fast = nextsum(nextsum(fast));
-        }
-        return fast == 1;
-    }
+    // bool isHappy(int n) {
+    //     int fast = nextsum(n), slow = n;
+    //     while(fast != 1 && fast != slow){
+    //         slow = nextsum(slow);
+    //         fast = nextsum(nextsum(fast));
+    //     }
+    //     return fast == 1;
+    // }
     int nextsum(int a){
         int sum = 0;
         while(a){
@@ -16,5 +16,16 @@ public:
             a = a/10;
         }
         return sum;
+    }
+    bool isHappy(int n){
+        unordered_set<int> s;
+        int temp = n;
+        while(temp){
+            int sum = nextsum(temp);
+            if(s.count(sum)) return false;
+            if(sum == 1) return true;
+            s.insert(sum);
+            temp = sum;
+        } return false;
     }
 };
